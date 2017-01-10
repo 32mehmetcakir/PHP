eb239ea9a3cce43844d4fd8ec4eee219d8c636de
memo242 <morkoyun2004@mynet.com>
2017-01-10 23:02:43 +0200

    latest work stable
    tamamıyla revize edilmiş ve sayfa refresh koduda eklendi. kod temizliği yapıldı. icabında curl timeout süresi kısaltılır 20 saniye geri çekilebilir.

--- php_curl_remotedownload.php
+++ php_curl_remotedownload.php
@@ -1,37 +1,22 @@
 <?php
 # Kaynak : http://denizyildirim.net/2013/05/php-curl-ile-yarim-downloadlari-tamamlamak/
 # Kaynak2: http://mfyz.com/php-ile-curl-kutuphanesinin-kullanimi
-/**
-    Download remote file in php using curl
-    Files larger that php memory will result in corrupted data
-*/
+
 session_start(); //to ensure you are using same session
 ob_start(); // start output buffer
 # set_time_limit(40);
 ini_set('max_execution_time', 40);
 ini_set('memory_limit', '512M');
+
+// Timeout
+$startTime = time();
+$timeout = 29;   //timeout in seconds
+
 echo "<pre>";
-echo "Loading ...<br />";
-# ob_start();
+echo "<b>Loading ...<br /></b>";
 # ob_flush();
 # flush();
-function progress($resource, $download_size, $downloaded, $upload_size, $uploaded)
-{
-    if ($download_size > 0)
-	{
-        #$progress = round($downloaded / $download_size * 100);
-		$progress = $downloaded / $download_size  * 100;
-	}
-    /*
-	$progress = array('progress' => $progress);
-    $path = "./";
-    $destination = $path."11.json";
-    $file = fopen($destination, "w+");
-	$json_encode = json_encode($progress, JSON_UNESCAPED_UNICODE);
-    fwrite($file, $json_encode);
-    fclose($file);
-	*/
-}
+
 	/**
 	* Get Remote File Size
 	*
@@ -48,8 +33,7 @@ function remote_file_size($url)
     return (int) $data['Content-Length'];
 }
 
-# $urlfile = "http://www.mega-debrit.com/index.php/MEGA/mega_eab4ba20d6/www.mega-debrit.com_MT6572__alps___tangxun6572_we_l__5.1__ALPS.L1.MP6.V2.8_TANGXUN6572.WE.L.rar";
-$urlfile = "https://doughty-surprise.000webhostapp.com/MT6572__alps___tangxun6572_we_l__5.1__ALPS.L1.MP6.V2.8_TANGXUN6572.WE.L.rar";
+$urlfile = "http://www.mega-debrit.com/index.php/MEGA/mega_eab4ba20d6/www.mega-debrit.com_MT6572__alps___tangxun6572_we_l__5.1__ALPS.L1.MP6.V2.8_TANGXUN6572.WE.L.rar";
 $fileName = "leyla.rar";
 echo "Local File Name	: " .$fileName . "<br />";
 echo "Remote URL	: " .$urlfile . "<br />";
@@ -64,7 +48,7 @@ if (file_exists($fileName))
 	echo "RemoteSize	: " . $remotefrom . " Byte<br />";
 	if ($from == $remotefrom)
 	{
-		echo "Dosya zaten inmiş. ==> İşlem yapılmadı! ==> Remote Size == Local Size birbirine eşit :)";
+		echo "<b>Dosya zaten inmiş. ==> İşlem yapılmadı! ==> Remote Size == Local Size birbirine eşit :)</b>";
 		curl_close($ch); // Curl işlemini bitir
 		exit;
 	}
@@ -79,31 +63,25 @@ if (file_exists($fileName))
 	}	
 	curl_setopt($ch, CURLOPT_RANGE, $from . "-"); // ne kadarı indiyse o noktadan downloada başla
 }
-# $fp = fopen ($fileName, 'a+'); // İndirdiğimiz dosyamızın kaldığı yerden tamamlayacak şekilde aç
-$fp = fopen ($fileName, 'a'); // İndirdiğimiz dosyamızın kaldığı yerden tamamlayacak şekilde aç
+$fp = fopen ($fileName, 'a'); // İndirdiğimiz dosyamızın kaldığı yerden tamamlayacak şekilde aç (Default değeri 'a+' dır.)
 // Servera hangi browser kullandığımızı gönderelim
 # curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36');
 @chmod($fileName, 0755); // kayıtlı dosyaya yazma hakkımız yoksa o hakkı verelim
-curl_setopt($ch, CURLOPT_TIMEOUT, 4); // download işlemi için ne kadar uğraşsın (Default: 1950 saniye)
-# curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'progress'); // Progress-bar fonksiyonumuz
-curl_setopt($ch, CURLOPT_NOPROGRESS, true); // üstteki fonksiyonun çalışması için
+curl_setopt($ch, CURLOPT_TIMEOUT, 20); // download işlemi için ne kadar uğraşsın (Default: 1950 saniye)
+## curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'progress'); // Progress-bar fonksiyonumuz
+curl_setopt($ch, CURLOPT_NOPROGRESS, true); // üstteki fonksiyonun çalışması için (Default: false)
 curl_setopt($ch, CURLOPT_FILE, $fp); // Curl işleminin dosya download olduğunu belirtiyoruz
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // yönlendirme varsa takip et
-# curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Yorum satırı olarak kalmalı 0 kb sorunu buradan kaynaklanıyor.
-# curl_setopt($ch, CURLOPT_BUFFERSIZE, 4096);	// New
+## curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Yorum satırı olarak kalmalı 0 kb sorunu buradan kaynaklanıyor.
 
-$data = curl_exec($ch); // Curl işlemine başla
+curl_exec($ch); // Curl işlemine başla
 $curl_dump = curl_getinfo($ch); // İstatistik değerlerini dizi şeklinde al
 $dlhizi = $curl_dump['speed_download']; // curl_getinfo($ch, CURLINFO_SPEED_DOWNLOAD);	// New byte/sn cinsinden
 curl_close($ch); // Curl işlemini bitir
 fclose($fp); // Açtığımız dosyayı kapat.
-
-/* baglantiyi kapa
-    $islem = fopen($fileName, "a");
-    fwrite($islem, $data);
-    fclose($islem);
-// Açtığımız dosyayı kapat. */
+// 2 saniye bekle
+usleep(2000000);
 chmod($fileName, 0644); // Dosya yazma iznini kapat.
 
 echo "Local Size	: ". filesize($fileName). " Byte\n";
@@ -112,17 +90,19 @@ echo "Tamamlanma Yüzdesi: %" .$tamamlanma_yuzdesi ."<br />";
 echo "Download Hızı	: " .(int)$dlhizi / 1024 . " KB/sn<br />";
 echo "<p><b>ÖZET:</b></p></br>";
 print_r($curl_dump); // Detaylı curl özeti
-/*
-	if ((int)$icerik < 100) 
-		{		
-		session_unset();
-		session_destroy(); //destroy the session
-		$delay=1; //Where 0 is an example of time Delay you can use 5 for 5 seconds for example !
-		#header("Refresh: $delay;"); 
-		exit();
+
+// Tamamlanma yüzdesi eksikse sayfa refresh edilecek
+	if ($tamamlanma_yuzdesi < 100) 
+		{
+		// Timeout buraya gelsin			
+		$delay=($startTime + $timeout) - time(); //Where 0 is an example of time Delay you can use 5 for 5 seconds for example !
+		echo "\n <b>$delay saniye sonra sayfa otomatik olarak REFRESH edilecektir...</b>\n";
+		echo "<center><b>Lütfen beklemeye devam ediniz!!!</b></center>\n";
+		// Timeout buraya gelsin SON
+		header("Refresh: $delay;");
 		}
-		*/
-echo "<b>Done</b>";
+// refresh son..	
+echo "<b>Done</b></pre>";
 ob_end_flush(); // Çıktı tamponunu temizler (siler) ve tamponu kapatır
 session_unset();
 session_destroy(); //destroy the session
