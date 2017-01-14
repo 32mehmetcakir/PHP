a0d53a68f9a622f91e8bc827b3667f3b320d9d85
memo242 <morkoyun2004@mynet.com>
2017-01-14 13:13:01 +0200

    parolalı zip dosyalarını açan php script
    İlk sürüm: Parolalı zip dosyalarının php kodları ile açılması sağlandı.

---
+++ zip-password-extract.php
@@ -0,0 +1,19 @@
+<?php
+    $zip = new ZipArchive();
+    $zip_status = $zip->open("MT6572__Samsung__SM-J500HDS__Samsung_J500HDS__4.4.2__ALPS.KK1.MP7.V1.zip");
+
+    if ($zip_status === true)
+    {
+        if ($zip->setPassword("keytak72"))
+        {
+            if (!$zip->extractTo(__DIR__))
+                echo "Extraction failed (wrong password?)";
+        }
+        $zip->close();
+        echo "Başarılı bir şekilde açıldı.";
+    }
+    else
+    {
+        die("Failed opening archive: ". @$zip->getStatusString() . " (code: ". $zip_status .")");
+    }
+?>
