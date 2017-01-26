fc6cc69cb2f37fdc822cdcfda4e1928f1b0646c5
memo242 <morkoyun2004@mynet.com>
2017-01-26 12:42:05 +0200

    $valid_exts add rar and zip
    "Invalid URL" sorunun çözümü için, $valid_exts değişkenine "rar","zip" değerleri eklendi.

--- RemoteFile-Upload-Download/file-uploader-script/upload.php
+++ RemoteFile-Upload-Download/file-uploader-script/upload.php
@@ -6,7 +6,7 @@ if($url)
 {
     $file = fopen($url,"rb");
     $directory = "upload/";
-    $valid_exts = array("php","jpeg","gif","png","doc","docx","jpg","html","asp","xml","JPEG","bmp"); 
+    $valid_exts = array("php","jpeg","gif","png","doc","docx","jpg","html","asp","xml","JPEG","bmp","rar","zip"); 
     $ext = end(explode(".",strtolower(basename($url))));
     if(in_array($ext,$valid_exts))
     {
