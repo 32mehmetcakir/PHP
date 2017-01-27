9cc7a18d45ab773f60d2e2b3eeffce4ef8f2d42c
memo242 <morkoyun2004@mynet.com>
2017-01-27 23:54:32 +0200

    Add memory_limit, max_execution_time, Fix $filea['data']
    Add ini_set('max_execution_time', 0);
    Add ini_set('memory_limit', '1024M');
    Renamed to $file['data'] --> $filea['data']
    şeklinde düzenlemeler yapıldı.

--- Rar-Zip-And-Others-Archives/ZipFile/example2.php
+++ Rar-Zip-And-Others-Archives/ZipFile/example2.php
@@ -1,5 +1,7 @@
 <?php
 require "zip.class.php"; // Get the zipfile class
+ini_set('max_execution_time', 0);
+ini_set('memory_limit', '1024M');
 $zipfile = new zipfile; // Create an object
 $zipfile->read_zip("myzip.zip"); // Read the zip file
 
@@ -8,6 +10,6 @@ $zipfile->read_zip("myzip.zip"); // Read the zip file
 
 foreach($zipfile->files as $filea)
 {
-	echo "The contents of {$filea['name']}:\n{$file['data']}\n\n";
+	echo "The contents of {$filea['name']}:\n{$filea['data']}\n\n";
 }
 ?>
