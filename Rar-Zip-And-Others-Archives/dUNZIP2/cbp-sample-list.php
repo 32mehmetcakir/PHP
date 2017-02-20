734bb9610122b9583512662f957bc92779d68cff
memo242 <morkoyun2004@mynet.com>
2017-02-20 14:35:58 +0200

    Create cbp-sample-list.php
    * cbp-sample-unzip.php dosyasından türetildi.
    * Sadece zip dosyasının içeriğini ayrıntılı listeleme yapar. (compressed size, uncompressed size, compressed method, date, offset, version, crc, gibi bilgileri sütun halinde listeler)

---
+++ Rar-Zip-And-Others-Archives/dUNZIP2/cbp-sample-list.php
@@ -0,0 +1,19 @@
+<?
+require_once dirname(__FILE__)."/dUnzip2.inc.php";
+
+ini_set('max_execution_time', 0);
+ini_set('memory_limit', '2024M');
+
+######## Then, load the file again. Now, to unzip it ########
+echo "<hr>";
+
+$zipfilepath = '../grabber/babo.zip';	# Zip dosyasının adresi
+$zip = new dUnzip2($zipfilepath);
+
+// Activate debug
+$zip->debug = true;
+
+// Unzip all the contents of the zipped file to a new folder called "uncompressed"
+$zip->getList();
+$zip->close();
+?>
